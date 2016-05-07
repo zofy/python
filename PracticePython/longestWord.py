30688dd4c0f42b0d2316b0a3406ff2a0b7ea7a75
zofy <zofy11@gmail.com>
2016-05-07 17:30:18 +0200

    longestWord - os.path.join / os.path.isFile

--- PracticePython/longestWord.py
+++ PracticePython/longestWord.py
@@ -1,4 +1,5 @@
 import glob
+import os
 
 
 def find_longest(dir):
@@ -44,5 +45,32 @@ def find_longest2(dir):
         print output
 
 
+def find_longest3(dir):
+    try:
+        files = os.listdir(dir)
+    except Exception:
+        print 'No such a directory!'
+    else:
+        output = {}
+        for file in files:
+            file_path = os.path.join(dir, file)
+            if os.path.isfile(file_path):
+                output[file] = sorted(open(file_path).read().split(),
+                                      key=lambda word: len([x for x in word if 97 <= ord(x) <= 122 or 65 <= ord(x) <= 90]),
+                                      reverse=True)[0]
+        print output
+
+
+dir = 'C:\Users\Patrik\Desktop\Test'
+
+
+def longest_word(filename):
+    return sorted(open(filename).read().split(), key=len, reverse=True)[0]
+
+
+print {file_name: longest_word(os.path.join(dir, file_name)) for file_name in os.listdir(dir) if
+       os.path.isfile(os.path.join(dir, file_name))}
+
 # find_longest('Test')
-find_longest2('C:\Users\Patrik\Desktop\Test\\')
+# find_longest2('C:\Users\Patrik\Desktop\Test\\')
+find_longest3(dir)
